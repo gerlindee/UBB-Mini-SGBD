6b387f0b9ee43a533d093a618239d7aed567eda4
Gerlinde Christine <gerlindeciocea@yahoo.com>
2020-10-10 20:59:54 +0300

    get all databases method done

--- miniSGBD/ClientApp/TestForm.Designer.cs
+++ miniSGBD/ClientApp/TestForm.Designer.cs
@@ -32,6 +32,8 @@
             this.text_db_name = new System.Windows.Forms.TextBox();
             this.button_db_name = new System.Windows.Forms.Button();
             this.button_db_delete = new System.Windows.Forms.Button();
+            this.button_db_show_all = new System.Windows.Forms.Button();
+            this.button_db_show_all_tables = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label_db_name
@@ -55,9 +57,9 @@
             // button_db_name
             // 
             this.button_db_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button_db_name.Location = new System.Drawing.Point(403, 216);
+            this.button_db_name.Location = new System.Drawing.Point(231, 216);
             this.button_db_name.Name = "button_db_name";
-            this.button_db_name.Size = new System.Drawing.Size(104, 31);
+            this.button_db_name.Size = new System.Drawing.Size(133, 31);
             this.button_db_name.TabIndex = 2;
             this.button_db_name.Text = "Create";
             this.button_db_name.UseVisualStyleBackColor = true;
@@ -66,13 +68,35 @@
             // button_db_delete
             // 
             this.button_db_delete.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button_db_delete.Location = new System.Drawing.Point(403, 253);
+            this.button_db_delete.Location = new System.Drawing.Point(370, 216);
             this.button_db_delete.Name = "button_db_delete";
-            this.button_db_delete.Size = new System.Drawing.Size(104, 31);
+            this.button_db_delete.Size = new System.Drawing.Size(137, 31);
             this.button_db_delete.TabIndex = 4;
             this.button_db_delete.Text = "Delete";
             this.button_db_delete.UseVisualStyleBackColor = true;
             this.button_db_delete.Click += new System.EventHandler(this.button_db_delete_Click);
+            // 
+            // button_db_show_all
+            // 
+            this.button_db_show_all.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_db_show_all.Location = new System.Drawing.Point(231, 253);
+            this.button_db_show_all.Name = "button_db_show_all";
+            this.button_db_show_all.Size = new System.Drawing.Size(276, 31);
+            this.button_db_show_all.TabIndex = 5;
+            this.button_db_show_all.Text = "Display all Databases";
+            this.button_db_show_all.UseVisualStyleBackColor = true;
+            this.button_db_show_all.Click += new System.EventHandler(this.button_db_show_all_Click);
+            // 
+            // button_db_show_all_tables
+            // 
+            this.button_db_show_all_tables.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_db_show_all_tables.Location = new System.Drawing.Point(231, 290);
+            this.button_db_show_all_tables.Name = "button_db_show_all_tables";
+            this.button_db_show_all_tables.Size = new System.Drawing.Size(276, 31);
+            this.button_db_show_all_tables.TabIndex = 6;
+            this.button_db_show_all_tables.Text = "Display all Tables";
+            this.button_db_show_all_tables.UseVisualStyleBackColor = true;
+            this.button_db_show_all_tables.Click += new System.EventHandler(this.button_db_show_all_tables_Click);
             // 
             // TestForm
             // 
@@ -80,6 +104,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_db_show_all_tables);
+            this.Controls.Add(this.button_db_show_all);
             this.Controls.Add(this.button_db_delete);
             this.Controls.Add(this.button_db_name);
             this.Controls.Add(this.text_db_name);
@@ -97,6 +123,8 @@
         private System.Windows.Forms.TextBox text_db_name;
         private System.Windows.Forms.Button button_db_name;
         private System.Windows.Forms.Button button_db_delete;
+        private System.Windows.Forms.Button button_db_show_all;
+        private System.Windows.Forms.Button button_db_show_all_tables;
     }
 }
 
---
+++ miniSGBD/ServerApp/DatabaseManager.cs
@@ -0,0 +1,54 @@
+﻿using ServerApp.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using Utils;
+
+namespace ServerApp
+{
+    static class DatabaseManager
+    {
+        public static string ExecuteCommand(string command)
+        {
+            var commandSplit = command.Split(';');
+            var executionResponse = ""; 
+            switch (commandSplit[0])
+            {
+                case Commands.GET_ALL_DATABASES:
+                    {
+                        executionResponse = FetchDatabases();
+                    }
+                    break;
+                case Commands.CREATE_DATABASE:
+                    {
+                        executionResponse = new CreateDatabaseQuery(commandSplit[1]).Execute();
+                    }
+                    break;
+                case Commands.DROP_DATABASE:
+                    {
+                        executionResponse = new DropDatabaseQuery(commandSplit[1]).Execute();
+                    }
+                    break;
+            }
+            return executionResponse;
+        }
+
+        private static string FetchDatabases()
+        {
+            var databaseNames = Responses.GENERAL_DISPLAY_ENTRIES + ';';
+            var xmlDocument = new XmlDocument();
+            xmlDocument.Load(Application.StartupPath + "\\SGBDCatalog.xml");
+            XmlElement xmlRoot = xmlDocument.DocumentElement;
+
+            foreach (XmlNode childNode in xmlRoot.ChildNodes)
+            {
+                databaseNames = databaseNames + childNode.Attributes.GetNamedItem("databaseName").Value + '|';
+            }
+            return databaseNames;
+        }
+    }
+}

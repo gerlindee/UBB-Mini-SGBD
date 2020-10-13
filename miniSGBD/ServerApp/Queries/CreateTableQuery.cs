2f520d6e949b666dde953dc233789bceea09ccc6
Gerlinde Christine <gerlindeciocea@yahoo.com>
2020-10-13 21:57:18 +0300

    fixed an error

--- miniSGBD/ServerApp/Queries/CreateTableQuery.cs
+++ miniSGBD/ServerApp/Queries/CreateTableQuery.cs
@@ -160,7 +160,7 @@ namespace ServerApp.Queries
                     foreach (TableColumn foreignKey in GetPrimaryKeysOfGivenTable(reference, givenDatabaseNode))
                     {
                         XElement referencedColumn = new XElement("ReferencedColumn", foreignKey.Name);
-                        referencesNode.Add(referencedColumn);
+                        foreignKeyNode.Add(referencedColumn);
 
                         // Also add the PK field to the table structure as a column
                         structureNode.Add( new XElement("Column",

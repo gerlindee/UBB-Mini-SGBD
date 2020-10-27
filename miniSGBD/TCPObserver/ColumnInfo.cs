e0b82ffed175e6e81f7e9c4b209a6c57de798e0f
Ale Ardelian <alexis07oana@yahoo.com>
2020-10-27 19:49:17 +0200

    Delete context menu

--- miniSGBD/TCPObserver/ColumnInfo.cs
+++ miniSGBD/TCPObserver/ColumnInfo.cs
@@ -8,13 +8,61 @@ namespace Utils
 {
     public class ColumnInfo
     {
+        public ColumnInfo(string command)
+        {
+            var columnStruct = command.Split('#');
+            ColumnName = columnStruct[0];
+
+            for (var i = 1; i < columnStruct.Length; i++)
+            {
+                switch (columnStruct[i])
+                {
+                    case ColumnInformation.PK:
+                        {
+                            PK = true;
+                        }
+                        break;
+                    case ColumnInformation.FK:
+                        {
+                            FK = true;
+                        }
+                        break;
+                    case ColumnInformation.UNQ:
+                        {
+                            Unique = true;
+                        }
+                        break;
+                    case ColumnInformation.NULL:
+                        {
+                            NonNull = true;
+                        }
+                        break;
+                    default:
+                        {
+                            try
+                            {
+                                var types = columnStruct[i].Split('-');
+                                Type = types[0];
+                                int.TryParse(types[1], out int val);
+                                Lenght = val;
+                            }
+                            catch (Exception)
+                            {
+                                Type = columnStruct[i];
+                            }
+                        }
+                        break;
+                }
+            }
+        }
+
         public string ColumnName {get; set;}
         public bool PK { get; set; } = false;
         public bool FK { get; set; } = false;
         public string FKReferenceTable { get; set; }
-        public bool unique { get; set; } = false;
-        public bool nonNull { get; set; } = false;
-        public string type { get; set; }
-        public int lenght { get; set; } = -1;
+        public bool Unique { get; set; } = false;
+        public bool NonNull { get; set; } = false;
+        public string Type { get; set; }
+        public int Lenght { get; set; } = -1;
     }
 }

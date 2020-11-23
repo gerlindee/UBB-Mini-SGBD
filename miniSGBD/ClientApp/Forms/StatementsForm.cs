2489009d7571cea46dbc94cc0b6a44bbf95c4ca9
Ale Ardelian <alexis07oana@yahoo.com>
2020-11-23 18:06:27 +0200

    Create window for statements

---
+++ miniSGBD/ClientApp/Forms/StatementsForm.cs
@@ -0,0 +1,26 @@
+﻿using ClientApp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace miniSGBD.Forms
+{
+    public partial class StatementsForm : Form
+    {
+        private string databaseName;
+        private Client tcpClient;
+
+        public StatementsForm(string _databaseName, Client _tcpClient)
+        {
+            databaseName = _databaseName;
+            tcpClient = _tcpClient;
+            InitializeComponent();
+        }
+    }
+}

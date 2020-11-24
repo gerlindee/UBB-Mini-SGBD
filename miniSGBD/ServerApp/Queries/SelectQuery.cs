0d5924262c28a544165c96f128c080935a943a22
Ale Ardelian <alexis07oana@yahoo.com>
2020-11-24 20:33:17 +0200

    Mda

--- miniSGBD/ServerApp/Queries/SelectQuery.cs
+++ miniSGBD/ServerApp/Queries/SelectQuery.cs
@@ -7,24 +7,51 @@ using Utils;
 
 namespace ServerApp.Queries
 {
-    class SelectQuery
+    class SelectQuery : AbstractQuery
     {
         private string DatabaseName;
         private string TableName;
+        private string Attributes;
         private MongoDBAcess MongoDB;
 
-        public SelectQuery(string _databaseName, string _tableName)
+        private List<SelectRowInfo> SelectRows = new List<SelectRowInfo>();
+        private List<Tuple<string,string>> OutputParamsAndAlias = new List<Tuple<string, string>>();
+        private List<Tuple<string, string>> FilterParamsAndCondition = new List<Tuple<string, string>>();
+        private List<string> GroupByList = new List<string>();
+
+        public SelectQuery(string _databaseName, string _tableName, string _attributes) : base(Commands.SELECT_RECORDS)
         {
             DatabaseName = _databaseName;
             TableName = _tableName;
+            Attributes = _attributes;
             MongoDB = new MongoDBAcess(DatabaseName);
         }
 
-        public string Execute()
+        public override void ParseAttributes()
         {
-            return SelectEntireTable();
+            var rows = new List<SelectRowInfo>();
+            var splitAttributes = Attributes.Split('|');
+
+            foreach(var attribute in splitAttributes)
+            {
+                var selectRowInfo = new SelectRowInfo(attribute);
+                SelectRows.Add(selectRowInfo);
+            }
         }
 
+        public void parseRows()
+        {
+            foreach(var row in SelectRows)
+            {
+                if (row.Alias != "-")
+                    OutputParamsAndAlias.Add(new Tuple<string, string>(row.ColumnName, row.Alias));
+                if (row.Filter != "-")
+                    FilterParamsAndCondition.Add(new Tuple<string, string>(row.ColumnName, row.Filter));
+                if (row.GroupBy)
+                    GroupByList.Add(row.ColumnName);
+
+            }
+        }
         private string SelectEntireTable()
         {
             try

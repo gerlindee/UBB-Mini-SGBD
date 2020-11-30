504bd4fff3ed87c84052cac8a4d9748a01ce33a4
Gerlinde Christine <gerlindeciocea@yahoo.com>
2020-11-30 23:51:47 +0200

    merged the two select query classes part 2

--- miniSGBD/ServerApp/Queries/SelectQuery.cs
+++ miniSGBD/ServerApp/Queries/SelectQuery.cs
@@ -14,7 +14,11 @@ namespace ServerApp.Queries
         private MongoDBAcess MongoDB;
 
         private string SelectAllTableName;
-        private List<SelectRowInfo> SelectConfig = new List<SelectRowInfo>();
+        private List<SelectRowInfo> SelectConfigList = new List<SelectRowInfo>();
+        private List<Tuple<Tuple<string, string>, string>> OutputParamsAliasList = new List<Tuple<Tuple<string, string>, string>>();
+        private List<Tuple<Tuple<string, string>, string>> WhereConditionsList = new List<Tuple<Tuple<string, string>, string>>();
+        private List<Tuple<Tuple<string, string>, string>> HavingParamsList = new List<Tuple<Tuple<string, string>, string>>();
+        private List<Tuple<string, string>> GroupByList = new List<Tuple<string, string>>();
 
         public SelectQuery(string _databaseName, string _attributes) : base(Commands.SELECT_RECORDS)
         {
@@ -30,6 +34,44 @@ namespace ServerApp.Queries
             if (splitAttributes[0].Contains("SELECT_ALL"))
             {
                 SelectAllTableName = splitAttributes[0].Split('#')[1];
+            }
+            else
+            {
+                foreach (var attribute in splitAttributes)
+                {
+                    if (attribute != "")
+                    {
+                        SelectConfigList.Add(new SelectRowInfo(attribute));
+                    }
+                }
+
+                ParseSelectConfig();
+            }
+        }
+
+        private void ParseSelectConfig()
+        {
+            foreach (var column in SelectConfigList)
+            { 
+                if (column.Output)
+                {
+                    OutputParamsAliasList.Add(new Tuple<Tuple<string, string>, string>(new Tuple<string, string>(column.TableName, column.ColumnName), column.Alias));
+                }
+
+                if (column.Filter != "-")
+                {
+                    WhereConditionsList.Add(new Tuple<Tuple<string, string>, string>(new Tuple<string, string>(column.TableName, column.ColumnName), column.Filter));
+                }
+
+                if (column.GroupBy)
+                {
+                    GroupByList.Add(new Tuple<string, string>(column.TableName, column.ColumnName));
+                }
+
+                if (column.Having != "")
+                {
+                    HavingParamsList.Add(new Tuple<Tuple<string, string>, string>(new Tuple<string, string>(column.TableName, column.ColumnName), column.Having));
+                }
             }
         }
 
@@ -65,5 +107,38 @@ namespace ServerApp.Queries
                 return ex.Message + ";";
             }
         }
+
+        /*
+        private void checkWhereInIndexFiles()
+        {
+            var mongoDB = new MongoDBAcess(DatabaseName);
+
+            string whereAttributes = "";//Where has priority
+            foreach (var whereCondition in FilterParamsAndCondition)
+            {
+                whereAttributes += whereCondition.Item1 + '#';
+            }
+            whereAttributes = whereAttributes.Remove(whereAttributes.Length - 1);
+
+            var pkOfWhere = MongoDB.GetRecordValueWithKey(TableName, whereAttributes);
+
+            if (pkOfWhere.Length != 0)
+            {
+                try
+                {
+                    var listOfPk = pkOfWhere.Split('#');
+                    foreach (var pk in listOfPk)
+                    {
+                        var valueFromMainList = MongoDB.GetRecordValueWithKey(TableName, pk);
+                    }
+                }
+                catch (Exception e)
+                {
+
+                }
+
+            }
+        }
+        */
     }
 }
